c012f77970a88cf715040635817b90f9646f2e99
Basistyi Yevhen <psik726@gmail.com>
2020-10-24 00:28:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Udemy__OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int counter = 0;
+
+            for (int i = 1; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+
+                    counter++;
+                }
+            }
+            int[] arr = new int[] { sum };
+
+            if (arr[arr.Length - 1] == number)
+            {
+                
+                Console.WriteLine("given number is  a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("given number is not a perfect number");
+            }
+        }
+    }
+}
